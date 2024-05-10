b9ca4f2942ce820cf20413a07b186764f8c9f079
Sidahmed Belkadi <sidahmed.belkadi0@gmail.com>
2024-05-10 18:57:28 +0100

    First Commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PostsApp extends StatelessWidget {
+  const PostsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World, Posts App with clean architecture"),
+      ),
+    );
+  }
+}
