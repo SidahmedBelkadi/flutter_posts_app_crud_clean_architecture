c080e88983da6703046662bcb6b2184bde8646a6
Sidahmed Belkadi <sidahmed.belkadi0@gmail.com>
2024-05-11 11:31:53 +0100

    Created Posts Repo.

--- lib/features/posts/domain/repositories/post_repository.dart
+++ lib/features/posts/domain/repositories/post_repository.dart
@@ -1,1 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:posts_app/core/error/failure.dart';
+import 'package:posts_app/features/posts/domain/entities/post.dart';
 
+abstract class PostsRepository {
+  Future<Either<Failure, Unit>> createPost({required PostEntity postEntity});
+
+  Future<Either<Failure, List<PostEntity>>> getAllPosts();
+
+  Future<Either<Failure, Unit>> updatePost({required PostEntity postEntity});
+
+  Future<Either<Failure, Unit>> deletePost({required String id});
+}
